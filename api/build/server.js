eefb70430b797479c7b7efdda18e9cf595aa1499
Kirill Grachev <kgrachev96@gmail.com>
2018-02-07 17:08:37 +0300

    update server

--- api/build/server.js
+++ api/build/server.js
@@ -15,7 +15,6 @@ require("reflect-metadata");
 const typeorm_1 = require("typeorm");
 const api_1 = require("./api");
 const initialSettings_1 = require("./inits/initialSettings");
-//100.10.107.232
 const PORT = parseInt(process.env.PORT) || 3000;
 typeorm_1.createConnection().then((connection) => __awaiter(this, void 0, void 0, function* () {
     yield initialSettings_1.initSettings();
@@ -29,35 +28,4 @@ typeorm_1.createConnection().then((connection) => __awaiter(this, void 0, void 0
     });
     app.get("/", (req, res) => res.send("Server in LIVE!"));
 })).catch((error) => console.log(error));
-// const nextId = 4;
-// app.get("/api/todos", (req, res) => {
-//     res.send(todos);
-// });
-// app.post("/api/todos", (req, res) => {
-//     const todo = {
-//         id: nextId++,
-//         title: req.body.title,
-//         completed: false,
-//     };
-//     todos.push(todo);
-//     res.send(todo);
-// });
-// app.put("/api/todos/:id", (req, res) => {
-//     const todo = todos.find((todo) => todo.id === req.params.id);
-//     if (!todo) { return res.sendStatus(404); }
-//     todo.title = req.body.title || todo.title;
-//     res.json(todo);
-// });
-// app.patch("/api/todos/:id", (req, res) => {
-//     const todo = todos.find((todo) => todo.id === req.params.id);
-//     if (!todo) { return res.sendStatus(404); }
-//     todo.completed = !todo.completed;
-//     res.json(todo);
-// });
-// app.delete("/api/todos/:id", (req, res) => {
-//     const index = todos.findIndex((todo) => todo.id === req.params.id);
-//     if (index === -1) { return res.sendStatus(404); }
-//     todos.splice(index, 1);
-//     res.sendStatus(204);
-// }); 
 //# sourceMappingURL=server.js.map
--- api/src/server.ts
+++ api/src/server.ts
@@ -5,7 +5,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import api from "./api";
 import { initSettings } from "./inits/initialSettings";
-//100.10.107.232
+
 const PORT = parseInt(process.env.PORT) || 3000;
 
 createConnection().then(async (connection) => {
@@ -21,50 +21,3 @@ createConnection().then(async (connection) => {
     app.get("/", (req, res) => res.send("Server in LIVE!"));
 
 }).catch((error) => console.log(error));
-// const nextId = 4;
-
-// app.get("/api/todos", (req, res) => {
-//     res.send(todos);
-// });
-
-// app.post("/api/todos", (req, res) => {
-//     const todo = {
-//         id: nextId++,
-//         title: req.body.title,
-//         completed: false,
-//     };
-
-//     todos.push(todo);
-
-//     res.send(todo);
-// });
-
-// app.put("/api/todos/:id", (req, res) => {
-//     const todo = todos.find((todo) => todo.id === req.params.id);
-
-//     if (!todo) { return res.sendStatus(404); }
-
-//     todo.title = req.body.title || todo.title;
-
-//     res.json(todo);
-// });
-
-// app.patch("/api/todos/:id", (req, res) => {
-//     const todo = todos.find((todo) => todo.id === req.params.id);
-
-//     if (!todo) { return res.sendStatus(404); }
-
-//     todo.completed = !todo.completed;
-
-//     res.json(todo);
-// });
-
-// app.delete("/api/todos/:id", (req, res) => {
-//     const index = todos.findIndex((todo) => todo.id === req.params.id);
-
-//     if (index === -1) { return res.sendStatus(404); }
-
-//     todos.splice(index, 1);
-
-//     res.sendStatus(204);
-// });
